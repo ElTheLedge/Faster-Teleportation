405b1e84f2f3759e8ff7023fe0b6eb627260c08c
G4mer1808 <g4mer1808@gmail.com>
2021-02-19 08:06:54 +0100

    Add project files.

---
+++ FasterTeleportation.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace Faster_Teleportation
+{
+    
+
+    [BepInPlugin("com.lvh-it.valheim.fasterteleportation", "Faster Teleportation", "0.1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class FasterTeleportation : BaseUnityPlugin
+    {
+        void Main()
+        {
+            Harmony.CreateAndPatchAll(typeof(FasterTeleportation));
+            Logger.LogInfo("Patched Teleport Timer");
+        }
+
+        [HarmonyPatch(typeof(Player), "UpdateTeleport")]
+        [HarmonyPrefix]
+        static void patchTeleportTimer(ref float dt)
+        {
+            dt = 20f;
+        }
+
+        [HarmonyPatch(typeof(Player), "TeleportTo")]
+        [HarmonyPrefix]
+        static void makeTeleportNonDistant(ref bool distantTeleport)
+        {
+            distantTeleport = false;
+        }
+    }
+}
