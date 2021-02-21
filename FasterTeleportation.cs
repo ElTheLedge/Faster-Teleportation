3bb866a0d4d81264dcb1424e825039a77c217b8d
G4mer1808 <g4mer1808@gmail.com>
2021-02-21 21:53:48 +0100

    instant loading screen

--- FasterTeleportation.cs
+++ FasterTeleportation.cs
@@ -1,52 +1,60 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
 using HarmonyLib;
 using System.Diagnostics;
 
 namespace Faster_Teleportation
 {
-    
-
-    [BepInPlugin("com.lvh-it.valheim.fasterteleportation", "Faster Teleportation", "0.3.0.0")]
+    [BepInPlugin("com.lvh-it.valheim.fasterteleportation", "Faster Teleportation", "0.3.1.0")]
     [BepInProcess("valheim.exe")]
     public class FasterTeleportation : BaseUnityPlugin
     {
+        private static bool timerStarted = false;
+        private static Stopwatch stopwatch = new Stopwatch();
+        private static ManualLogSource logger;
+        private static bool playerIsTeleporting = false;
+
         void Main()
         {
+            logger = Logger;
+
             Harmony.CreateAndPatchAll(typeof(FasterTeleportation));
             Logger.LogInfo("Patched Teleport Timer");
         }
-
-        bool timerStarted = false;
-        Stopwatch stopwatch = new Stopwatch();
-        static FasterTeleportation FT = new FasterTeleportation();
 
         [HarmonyPatch(typeof(Player), "UpdateTeleport")]
         [HarmonyPrefix]
         static void patchTeleportTimer(ref bool ___m_teleporting, ref float ___m_teleportTimer, ref float dt)
         {
-            if(___m_teleporting)
+            playerIsTeleporting = ___m_teleporting;
+
+            if (___m_teleporting && !timerStarted)
             {
-                if(!FT.timerStarted)
-                {
-                    FT.stopwatch.Start();
-                    FT.timerStarted = true;
-                }
-            } 
-            else
+                stopwatch.Start();
+                timerStarted = true;
+            }
+            else if (!___m_teleporting && timerStarted)
             {
-                if(FT.timerStarted)
-                {
-                    FT.stopwatch.Stop();
-                    FT.Logger.LogInfo("Teleporting took "+ FT.stopwatch.ElapsedMilliseconds + " ms");
-                    FT.stopwatch.Reset();
-                    FT.timerStarted = false;
-                }
+                stopwatch.Stop();
+                logger.LogInfo("Teleporting took "+ stopwatch.ElapsedMilliseconds + " ms");
+                stopwatch.Reset();
+                timerStarted = false;
             }
 
             dt *= 3;
-            if (___m_teleportTimer > 0f && ___m_teleportTimer <= 2f)
+            if (___m_teleportTimer > 0f && ___m_teleportTimer < 2f)
             {
-                ___m_teleportTimer = 3f;
+                ___m_teleportTimer = 2f;
+            }
+        }
+
+        [HarmonyPatch(typeof(Hud), "UpdateBlackScreen")]
+        [HarmonyPrefix]
+        static void patchUpdateBlackScreen(ref UnityEngine.CanvasGroup ___m_loadingScreen)
+        {
+            if(playerIsTeleporting)
+            {
+                ___m_loadingScreen.alpha = 1f;
             }
         }
     }
